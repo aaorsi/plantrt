9c9d010043b2f32840f39f98d4f5dbfe39890e76
Alvaro Orsi <aaorsi@gmail.com>
2019-10-21 15:21:26 +1300

    Added leaves. Need debugging to ensure that they fir within bounding box

--- scene_conf.py
+++ scene_conf.py
@@ -26,7 +26,8 @@ def default_scene_elements(scene_extent):
             'name': [],
             'type': [],
             'normal':[],
-            'bounds': []
+            'bounds': [],
+            'canopy': []
             }
 
     
@@ -47,7 +48,8 @@ def default_scene_elements(scene_extent):
     gapx = scene_extent[ax_al] - cwidth*ntrees
 
     cgaps = gapx/ntrees 
-
+    
+    ibb = 0
     for i in range(ntrees):
         for j in range(nrows):
             # canopy
@@ -59,6 +61,9 @@ def default_scene_elements(scene_extent):
                                     [(cgaps+cwidth)*(0.5+i)+cwidth/2., 
                                         scene_extent[1]/2.+cwidth/2. - (2*j-(nrows-1))*cwidth*.75,
                                         scene_extent[2]*3/5.]])
+            
+            bboxes['canopy'].append(kiwi_tbar(bboxes, ibb))
+            
             # trunk
             bboxes['name'].append(f'trunk_{i}.{j}')
             bboxes['type'].append('bbox')
@@ -71,11 +76,16 @@ def default_scene_elements(scene_extent):
                 
             
 
+            bboxes['canopy'].append([]) 
+            ibb += 1
+            
+
+
     return bboxes
 
 
 def lad_0():
-    return 0, 0
+    return np.random.normal(0,np.pi/8.)
 
 
 def kiwi_tbar(bboxes, ibb, nleaves = 100, lad = lad_0):
@@ -85,16 +95,56 @@ def kiwi_tbar(bboxes, ibb, nleaves = 100, lad = lad_0):
     cane_sep = 40.0 # cane separation
     bar_height = 85.0 # height of structure
 
-    bb_sizex = bboxes['bounds'][ibb][1,0] - bboxes['bounds'][ibb][0,0]
-    bb_sizey = bboxes['bounds'][ibb][1,1] - bboxes['bounds'][ibb][0,1]
+    bb_sizex = bboxes['bounds'][ibb][1][0] - bboxes['bounds'][ibb][0][0]
+    bb_sizey = bboxes['bounds'][ibb][1][1] - bboxes['bounds'][ibb][0][1]
     
     cane_length0 = bb_sizey
     ncanes       = int(bb_sizex/cane_sep)
     coff         = bb_sizex - cane_sep*ncanes 
     nshoots_avg  = 5
     shoot_size   = cane_sep
+    lead_pos = bb_sizey/2.
+    leaf = {}
+    leaf['center'] = []
+    leaf['radius'] = []
+    leaf['normal'] = []
+    
+    kl = 0 # leaf counter
+    for ic in range(ncanes):
+        pos_cane = coff/2. + bboxes['bounds'][ibb][0][0] + ic*cane_sep # + np.random.normal(0.0,5.0)
+        
+        for ix in range(2):
+        # canes have different shoots in both sides wrt the main lead
+            nshoots = int(nshoots_avg)# + np.random.normal(0.0,3))
+            
+            for ish in range(nshoots):
+                sign = 1 if np.random.random() < 0.5 else -1
+                spos = np.random.uniform(0,cane_length0/2.)
+                ssize = shoot_size # + np.random.normal(0.0, shoot_size/2.)
+                leaves_shoot_half = int(5)# + np.random.normal(0.0, 5))
+                if leaves_shoot_half < 1:
+                    leaves_shoot_half = 1
 
-# TODO: generate kiwifruit leaves
+                lsep = int(ssize/leaves_shoot_half)
+                loff = ssize - lsep*leaves_shoot_half
 
+                for il in range(leaves_shoot_half):
+                    leaf_rad = 2.0 # + np.random.normal(0.0,2.0)
+                    lpos = loff/2. + lsep*il
+
+                    leaf['center'].append([pos_cane + lpos, lead_pos + ssize*sign, bar_height])
+                    leaf['radius'].append(leaf_rad)
+                    leaf['normal'].append([lad_0(), 2*np.pi*np.random.uniform()])
+
+                    leaf['center'].append([pos_cane - lpos, lead_pos + ssize*sign, bar_height])
+                    leaf['radius'].append(leaf_rad)
+                    leaf['normal'].append([lad_0(), 2*np.pi*np.random.uniform()])
+                    
+                    kl += 2
+                    
+
+    print (f'Number of leaves {kl}')
+    return leaf
+                
 
 
--- visualise.py
+++ visualise.py
@@ -9,6 +9,10 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 from matplotlib.patches import FancyArrowPatch
 from mpl_toolkits.mplot3d import proj3d
+from matplotlib.patches import Circle, PathPatch
+from matplotlib.transforms import Affine2D
+from mpl_toolkits.mplot3d import art3d
+
 
 
 def cuboid_data(o, size=(1,1,1)):
@@ -24,12 +28,14 @@ def cuboid_data(o, size=(1,1,1)):
     X += np.array(o)
     return X
 
+
+
 def plotCubeAt(positions,sizes=None,colors=None, **kwargs):
     #if not isinstance(colors,(list,np.ndarray)): colors=["C0"]*len(positions)
     #if not isinstance(sizes,(list,np.ndarray)): sizes=[(1,1,1)]*len(positions)
     g = []
     for p,s,c in zip(positions,sizes,colors):
-        g.append( cuboid_data(p, size=s) )
+        g.append(cuboid_data(p, size=s))
     return Poly3DCollection(np.concatenate(g),
                             facecolors=np.repeat(colors,6, axis=0), **kwargs)
 
@@ -47,4 +53,51 @@ class Arrow3D(FancyArrowPatch):
         FancyArrowPatch.draw(self, renderer)
 
 
+def rotation_matrix(d):
+    sin_angle = np.linalg.norm(d)
+    if sin_angle == 0:return np.identity(3)
+    d /= sin_angle
+    eye = np.eye(3)
+    ddt = np.outer(d, d)
+    skew = np.array([[    0,  d[2],  -d[1]],
+                  [-d[2],     0,  d[0]],
+                  [d[1], -d[0],    0]], dtype=np.float64)
 
+    M = ddt + np.sqrt(1 - sin_angle**2) * (eye - ddt) + sin_angle * skew
+    return M
+
+def pathpatch_2d_to_3d(pathpatch, z, normal):
+    if type(normal) is str: #Translate strings to normal vectors
+        index = "xyz".index(normal)
+        normal = np.roll((1.0,0,0), index)
+
+    normal /= np.linalg.norm(normal) #Make sure the vector is normalised
+    path = pathpatch.get_path() #Get the path and the associated transform
+    trans = pathpatch.get_patch_transform()
+
+    path = trans.transform_path(path) #Apply the transform
+
+    pathpatch.__class__ = art3d.PathPatch3D #Change the class
+    pathpatch._code3d = path.codes #Copy the codes
+    pathpatch._facecolor3d = pathpatch.get_facecolor #Get the face color    
+
+    verts = path.vertices #Get the vertices in 2D
+
+    d = np.cross(normal, (0, 0, 1)) #Obtain the rotation vector    
+    M = rotation_matrix(d) #Get the rotation matrix
+
+    pathpatch._segment3d = np.array([np.dot(M, (x, y, 0)) + (0, 0, z) for x, y in verts])
+
+def pathpatch_translate(pathpatch, delta):
+    pathpatch._segment3d += delta
+
+def plot_disk(ax, point, normal, size=10, color='y'):    
+    p = Circle((0, 0), size, facecolor = color, alpha=1.0,linewidth=1.0)
+    ax.add_patch(p)
+    pathpatch_2d_to_3d(p, z=0, normal=normal)
+    pathpatch_translate(p, (point[0], point[1], point[2]))
+
+
+o = np.array([5,5,5])
+v = np.array([3,3,3])
+n = [0.5, 0.5, 0.5]
